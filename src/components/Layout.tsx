e3f47b5df4a41dbe31e89b874e87ca34d431bef4
kldstone <kldstone.china@gmail.com>
2026-07-17 10:21:46 +0800

    fix: reset scroll position immediately

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import MobileStickyCTA from "./MobileStickyCTA";
@@ -9,9 +9,10 @@ import { trackPageview } from "@/lib/analytics";
 export default function Layout() {
   const location = useLocation();
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, [location.pathname]);
+  useLayoutEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }, [location.key]);
 
   useEffect(() => {
     trackPageview(location.pathname);

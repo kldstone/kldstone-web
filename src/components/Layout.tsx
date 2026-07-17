0c51043c3688b6a00a37886fe86f3854fc94e626
kldstone <kldstone.china@gmail.com>
2026-07-17 10:17:53 +0800

    feat: scroll to top on navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [location.pathname]);
 
   useEffect(() => {
